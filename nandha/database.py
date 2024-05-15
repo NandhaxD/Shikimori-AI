ce6b909e1f2e5328e12dd4b94e9c536eadca4103
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-16 00:35:54 +0530

    Update database.py

--- nandha/database.py
+++ nandha/database.py
@@ -2,9 +2,9 @@
 
 from nandha import db
 
+db = db['chats']
 
 def set_chat_mode(chat_id: int, mode):
-     db = db['chats']
      chat = {'chat_id': chat_id}
      db.update_one(chat,
             {'$set': {'chat': mode}}, upsert=True
@@ -12,7 +12,6 @@ def set_chat_mode(chat_id: int, mode):
      return True
 
 def get_chats():
-    db = db['chats']
     chat_ids = [ chat['chat_id'] for chat in db.find() ]
     if chat_ids is None:
         return []
@@ -21,7 +20,6 @@ def get_chats():
    
               
 def get_chat_mode(chat_id: int):
-      db = db['chats']
       chat = {'chat_id': chat_id}
       if not db.find_one(chat):
           set_chat_mode(chat_id, False)
