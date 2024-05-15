22223b57740a302080d01d959b1135a637ec627e
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-15 21:26:36 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+
+import os
+
+name = '𝗦 𝗛 𝗜 𝗞 𝗜 𝗠 𝗢 𝗥 𝗜'
+api_id = os.getenv('API_ID', None)
+api_hash = os.getenv('API_HASH', None)
+session = os.getenv('SESSION', None)
+db_url = os.getenv('DB_URL', None)
