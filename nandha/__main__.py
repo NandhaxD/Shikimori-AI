ebe441cf97eff3fc847597051836e5e58b9fee2a
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-15 21:40:43 +0530

    Create __main__.py

---
+++ nandha/__main__.py
@@ -0,0 +1,16 @@
+from nandha import shiki
+
+import pyrogram
+
+async def client()
+      await shiki.start()
+      await pyrogram.idle()
+      return await shiki.send_message(
+          chat_id='me', text='Hello, Shiki Restarted!')
+
+
+if __name__ == '__main__':
+    shiki.loop.run_until_complete(client)
+
+      
+      
