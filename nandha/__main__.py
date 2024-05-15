d11143e7be44a5c11f8ed810f829eb4eb1ec8914
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-15 23:53:37 +0530

    Update __main__.py

--- nandha/__main__.py
+++ nandha/__main__.py
@@ -3,15 +3,10 @@ from nandha import shiki
 import pyrogram
 import asyncio
 
-async def client():
-      await shiki.start()
-      await pyrogram.idle()
-      return await shiki.send_message(
-          chat_id='me', text='Hello, Shiki Restarted!')
 
 
 if __name__ == '__main__':
-    asyncio.run(client())
+      shiki.run()
 
       
       
