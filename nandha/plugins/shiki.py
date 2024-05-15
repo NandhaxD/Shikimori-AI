cba660afa0a498e3a8adab980743f925e2ace476
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-16 00:29:39 +0530

    Update shiki.py

--- nandha/plugins/shiki.py
+++ nandha/plugins/shiki.py
@@ -5,7 +5,7 @@ from pyrogram import filters
 import subprocess
 
 @shiki.on_message(filters.me & filters.command('logs'))
-async def logs(_, message)
+async def logs(_, message):
   logs = subprocess.getoutput('tail logs.txt')
   return await message.reply(
     text=logs)
