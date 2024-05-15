2dc79b661969b110a175364b79810880115063ee
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-16 02:30:07 +0530

    Update database.py

--- nandha/database.py
+++ nandha/database.py
@@ -27,7 +27,32 @@ def get_chat_mode(chat_id: int):
       return chat['chat']
       
            
-
+def get_chat_stickers(chat_id: int):
+    chat_js = {'chat_id': chat_id}
+    chat = db.find_one(chat_js)
+    if chat:
+         if 'stickers' in list(chat.keys()):
+             return chat['stickers']
+    else:
+         return []
+     
+def add_chat_sticker(chat_id: int, sticker_id):
+    chat_js = {'chat_id': chat_id}
+    chat = db.find_one(chat_js)
+    if 'stickers' in list(chat.keys()):
+           stickers = get_chat_stickers(chat_id)
+           if sticker_id in stickers:
+                 return
+         
+           else:
+                db.update_one(
+                     chat_js, {'$push': {'stickers': sticker_id}})
+                return True
+    else:
+         db.update_one(
+              chat_js, {'$set': {'stickers': [sticker_id]}})
+         return True
+         
           
-  
+
     
