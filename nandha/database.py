70bddcb5ed7ed57ea0e93e3d5d612defbf8e3572
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-15 22:13:58 +0530

    Create database.py
    Ok

---
+++ nandha/database.py
@@ -0,0 +1,35 @@
+
+
+from nandha import db
+
+
+def chat_mode(chat_id: int, mode):
+     db = db['chats']
+     chat = {'chat_id': chat_id}
+     db.update_one(chat,
+            {'$set': {'chat': mode}}, upsert=True
+                  )
+     return True
+
+def get_chats():
+    db = db['chats']
+    chat_ids = [ chat['chat_id'] for chat in db.find() ]
+    if chat_ids is None:
+        return []
+    else:
+        return chat_ids
+   
+              
+def get_chat_mode(chat_id: int):
+      db = db['chats']
+      chat = {'chat_id': chat_id}
+      if not db.find_one(chat):
+          chat_mode(chat_id, False)
+      chat = db.find_one(chat)
+      return chat['mode']
+      
+           
+
+          
+  
+    
